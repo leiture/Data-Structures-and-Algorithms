6e02e0862a24050c6bf2a26f6fb6b8f24756cc78
gongf <gongf@fxtcn.com>
2020-12-16 17:27:15 +0800

    tree and binary search tree

---
+++ binary_search_tree.py
@@ -0,0 +1,95 @@
+# -*-coding:utf:utf-8-*-
+# desc:binary sort tree
+import random
+
+
+class Tree:
+    """
+    二叉排序树、二叉搜索树、二叉查找树
+    对二叉查找树进⾏行行中序遍历，即可得到有序的数列列。
+    """
+
+    def __init__(self, v, left=None, right=None):
+        self.value = v
+        self.left = left
+        self.right = right
+
+    def insert(self, v, node):
+        """
+        插入
+        :param node: instance of Tree
+        :param v:
+        :return:
+        1) 若当前的⼆二叉查找树为空，则插⼊入的元素为根节点;
+        2) 若插⼊入的元素值⼩小于根节点值，则将元素插⼊入到左⼦子树中;
+        3) 若插⼊入的元素值不不⼩小于根节点值，则将元素插⼊入到右⼦子树中。
+        """
+        if node is None:
+            return Tree(v)
+        if node.value > v:
+            node.left = self.insert(v, node.left)
+        else:
+            node.right = self.insert(v, node.right)
+        return node
+
+    def remove(self, v, node):
+        if node is None:
+            return
+        if node.value > v:
+            node.left = self.remove(v, node.left)
+        elif node.value < v:
+            node.right = self.remove(v, node.right)
+        else:
+            if node.left and node.right:
+                node.value = self.find_mix(node.right).value
+                node.right = self.remove(node.value, node.right)
+            elif node.left:
+                node = node.left
+            elif node.right:
+                node = node.right
+            else:
+                return
+        return node
+
+    def contains(self, t, root):
+        """
+        查找
+        :param t:
+        :param root:
+        :return:
+        """
+        if not root:
+            return False
+        if t > root.value:
+            return self.contains(t, root.right)
+        elif t < root.value:
+            return self.contains(t, root.left)
+        else:
+            return True
+
+    def find_mix(self, root):
+        if not root:
+            return
+        if not root.left:
+            return root
+        return self.find_mix(root.left)
+
+    def find_max(self, root):
+        if not root:
+            return
+        if not root.right:
+            return root
+        return self.find_max(root.right)
+
+
+if __name__ == '__main__':
+    n = Tree(55)
+    for i in range(10):
+        n = n.insert(random.randint(0, 100), n)
+    n = n.insert(66, n)
+    n = n.insert(65, n)
+    n = n.insert(67, n)
+    print(n.find_mix(n).value)
+    print(n.find_max(n).value)
+    n = n.remove(66, n)
+    print(n)
---
+++ trees.py
@@ -0,0 +1,171 @@
+import random
+
+
+class Node:
+    """
+    二叉树类
+    """
+
+    def __init__(self, v=None):
+        self.value = v
+        self.left = None
+        self.right = None
+        self.h = 0
+
+
+def pre_order(root: Node):
+    """
+    递归实现
+    前序遍历二叉树   根节点-->左子树-->右子树
+    :param root:
+    :return:
+    """
+    if not root:
+        return
+    print(root.value, end=",")
+    pre_order(root.left)
+    pre_order(root.right)
+
+
+def pre_order_(root: Node):
+    """
+    非递归实现
+    前序遍历二叉树   根节点-->左子树-->右子树
+    :param root:
+    :return:
+    """
+    l = list()
+    while root or l:
+        while root:
+            print(root.value, end=",")
+            l.append(root)
+            root = root.left
+        root = l.pop(-1)
+        root = root.right
+    print()
+
+
+def pre_order_1(root: Node):
+    """
+    非递归实现
+    前序遍历二叉树   根节点-->左子树-->右子树
+    :param root:
+    :return:
+    """
+    l = list()
+    l.append(root)
+    while l:
+        cur = l.pop()
+        print(cur.value, end=",")
+        l.append(cur.right) if cur.right else None
+        l.append(cur.left) if cur.left else None
+    print()
+
+
+def mid_order(root: Node):
+    """
+    递归实现
+    中序遍历二叉树   左子树-->根节点-->右子树
+    :param node:
+    :return:
+    """
+    if root:
+        mid_order(root.left)
+        print(root.value, end=",")
+        mid_order(root.right)
+
+
+def mid_order_(root: Node):
+    """
+    非递归实现
+    中序遍历二叉树   左子树-->根节点-->右子树
+    :param node:
+    :return:
+    """
+    l = list()
+    while root or l:
+        while root:
+            l.append(root)
+            root = root.left
+        root = l.pop()
+        print(root.value, end=",")
+        root = root.right
+
+
+def last_order(root: Node):
+    """
+    递归
+    后序遍历二叉树   左子树 ---> 右子树 ---> 根结点
+    :param root:
+    :return:
+    """
+    if root:
+        last_order(root.left)
+        last_order(root.right)
+        print(root.value, end=",")
+
+
+def last_order_(root: Node):
+    """
+    非递归
+    后序遍历二叉树   左子树 ---> 右子树 ---> 根结点
+    :param root:
+    :return:
+    """
+    l1 = list()
+    l2 = list()
+    while l1:
+        node = l1.pop()
+        l1.append(node.left) if node.left else None
+        l1.append(node.right) if node.right else None
+        l2.append(node)
+    while l2:
+        node = l2.pop()
+        print(node.value, end=",")
+
+
+def level_order(root: Node):
+    """
+    层序遍历
+    只需按层次遍历即可
+    :param root:
+    :return:
+    """
+    l = list()
+    l.append(root)
+    while l:
+        node = l.pop(0)
+        print(node.value, end=",")
+        l.append(node.left) if node.left else None
+        l.append(node.right) if node.right else None
+
+
+def make_node(l=3, s=0):
+    """
+    生成树
+    节点数 2**(l+1)-1
+    :param l:
+    :return:
+    """
+    if not s:
+        s = l + 1
+    n = Node()
+    n.value = s - l
+    n.h = l + 1
+    if not l:
+        return n
+    n.left = make_node(l - 1, s)
+    n.right = make_node(l - 1
+                        , s+1)
+    return n
+
+
+if __name__ == '__main__':
+    s = make_node(2)
+    pre_order(s)
+    print()
+    mid_order(s)
+    print()
+    last_order(s)
+    print()
+    level_order(s)
